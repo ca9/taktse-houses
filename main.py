46f5d9fed6ef2ce21a43fa5df27b44299f77b27b
aditya <Aditya11009@iiitd.ac.in>
2015-06-07 10:20:52 +0530

    Aditya's checkin

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app.config['DEBUG'] = True
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return 'Hello World!'
+    return "Hello World! \n\n This is Aditya checking in."
 
 
 @app.errorhandler(404)
