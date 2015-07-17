2df7afbce37d629d89a89cc4c6933b5db3020874
aditya <Aditya11009@iiitd.ac.in>
2015-07-17 22:10:53 +0530

    CSS import example for Abhishek.

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
 app.config['DEBUG'] = True
+
+from flask import render_template
+from datetime import datetime
 
 # Note: We don't need to call run() since our application is embedded within
 # the App Engine WSGI application server.
@@ -22,3 +26,9 @@ def hello():
 def page_not_found(e):
     """Return a custom 404 error."""
     return 'Sorry, nothing at this URL.', 404
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hellopage(name="from Taktse"):
+    time_variable = datetime.now()
+    return render_template('hello.html', name=name, time=time_variable)
